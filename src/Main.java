073b8f0ab3442575d1392c794b51303d6976bd62
Zeeshan Jumabhoy <mustafazeeshan333@gmail.com>
2023-11-01 02:23:30 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+class sum{
+    public sum(){
+        int a=7;
+        int b=7;
+        System.out.println(a+b);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        sum obj=new sum();
+    }
+}
