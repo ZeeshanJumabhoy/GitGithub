580a06872792e00903a52754c8b22dddb405f2b2
Zeeshan Jumabhoy <mustafazeeshan333@gmail.com>
2023-11-01 02:28:09 +0500

    Done commiting

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,17 @@ class sum{
         System.out.println(a+b);
     }
 }
+class multiply{
+    public multiply(){
+        int a=7;
+        int b=7;
+        System.out.println(a*b);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         sum obj=new sum();
+        multiply ob=new multiply();
     }
 }
